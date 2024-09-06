46e7fa0507aca0bc7d7f69afbd90124405573eab
Ha Quanq <quangha0120010@gmail.com>
2024-09-06 13:20:37 +0700

    build: add custom colors variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,18 @@ export default {
       semibold: "var(--font-semibold)",
       black: "var(--font-black)",
     },
+    colors: {
+      blue: {
+        900: "var(--clr-blue-900)",
+        800: "var(--clr-blue-800)",
+        700: "var(--clr-blue-700)",
+      },
+      gray: {
+        100: "var(--clr-gray-100)",
+        200: "var(--clr-gray-200)",
+      },
+      white: "var(--clr-white)",
+    },
     extend: {},
   },
   plugins: [],
