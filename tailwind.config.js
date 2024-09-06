81d8673f291e5a188b0506d0c08379a6d11e6df1
Ha Quanq <quangha0120010@gmail.com>
2024-09-06 12:58:11 +0700

    build: add custom fontWeight variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: [],
   theme: {
     fontFamily: {},
+    fontWeight: {
+      thin: "var(--font-thin)",
+      semibold: "var(--font-semibold)",
+      black: "var(--font-black)",
+    },
     extend: {},
   },
   plugins: [],
