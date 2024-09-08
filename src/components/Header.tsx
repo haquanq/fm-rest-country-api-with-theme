94d8d65662ebbc9669669bc799cafdc6d3a437d5
Ha Quanq <quangha0120010@gmail.com>
2024-09-08 20:54:31 +0700

    feat: update Header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,8 +10,8 @@ export const Header = () => {
   };
 
   return (
-    <header>
-      <h1>Where in the world?</h1>
+    <header className="flex justify-between items-center pt-[39px] pb-9 px-5 shadow-lg">
+      <h1 className="font-semibold text-lg">Where in the world?</h1>
       <label
         className="flex items-center font-semibold cursor-pointer space-x-1"
         htmlFor="theme-switcher"
